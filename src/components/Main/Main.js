2d474f5f9c91827ef85706ea76c1282a5447db50
Joe Lee <kermitjosephlee@gmail.com>
2021-02-15 22:09:37 -0500

    added in <CaseStudy /> component and react-markdown package

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,28 +1,33 @@
 import React, { useState } from "react"
 import './Main.css'
 import { 
-  Header, 
-  Contact, 
   About, 
-  Profiles, 
-  Work, 
+  CaseStudy,
+  Contact, 
   Education, 
-  Volunteer, 
-  Skills, 
+  Header, 
   Languages, 
-  Interests 
+  Interests, 
+  Profiles, 
+  Skills,
+  Switcher, 
+  Volunteer, 
+  Work, 
 } from "./components"
 import { resume } from "../common"
 
 export default function Main (){
+  const [showResume, setShowResume] = useState(true) 
   const [isDarkMode, setIsDarkMode] = useState(false)
   const [isJson, setIsJson] = useState(false)
+
   const { basics, work, education, volunteer, skills, languages, interests } = resume
 
   return (
     <div className={isDarkMode ? "main dark" : "main"}>
           <Header basics={basics} isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} isJson={isJson} setIsJson={setIsJson} />
-      {!isJson && (
+          <Switcher showResume={showResume} setShowResume={setShowResume} />
+      {!isJson && showResume ? (
         <>
           <Contact basics={basics} />
           <About basics={basics} />
@@ -34,6 +39,8 @@ export default function Main (){
           <Languages languages={languages} />
           <Interests interests={interests} />
         </>
+      ) : (
+        <CaseStudy />
       )}
       {isJson && <div className="pre"><pre>{JSON.stringify(resume, null, 2)}</pre></div>}
     </div>
