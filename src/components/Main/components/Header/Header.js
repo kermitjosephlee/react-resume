e37c84cfa08b695cc4892c8e34dcdb9d2aaf35f4
Joe Lee <kermitjosephlee@gmail.com>
2021-03-01 14:48:24 -0500

    converted from Consumer to useContext hook

--- src/components/Main/components/Header/Header.js
+++ src/components/Main/components/Header/Header.js
@@ -1,34 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { DarkContext } from "../../../contexts/DarkContext";
 import "./Header.css";
 import "../Section/Section.css";
 
 export function Header({ basics, showJson }) {
+	const { isDarkMode, setIsDarkMode } = useContext(DarkContext);
 	const { name, label } = basics;
+
 	return (
-		<DarkContext.Consumer>
-			{({ isDarkMode, setIsDarkMode }) => {
-				return (
-					<div className="header">
-						<div
-							className="title-container"
-							onClick={() => setIsDarkMode(!isDarkMode)}
-						>
-							<img
-								className="headshot"
-								src={basics.picture}
-								alt="joe's headshot"
-								width="100"
-								height="100"
-							/>
-						</div>
-						<div className="name-container" onClick={() => showJson()}>
-							<div className="name">{name}</div>
-							<div className="label">{label}</div>
-						</div>
-					</div>
-				);
-			}}
-		</DarkContext.Consumer>
+		<div className="header">
+			<div
+				className="title-container"
+				onClick={() => setIsDarkMode(!isDarkMode)}
+			>
+				<img
+					className="headshot"
+					src={basics.picture}
+					alt="joe's headshot"
+					width="100"
+					height="100"
+				/>
+			</div>
+			<div className="name-container" onClick={() => showJson()}>
+				<div className="name">{name}</div>
+				<div className="label">{label}</div>
+			</div>
+		</div>
 	);
 }
