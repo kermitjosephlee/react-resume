54b6e80fde030ac80188e1be40c08e399d1b35c7
Joe Lee <kermitjosephlee@gmail.com>
2021-03-03 21:57:53 -0500

    promoted DarkProvider and ResumeProvider to App component to wrap Main component, updated isDarkMode from useContext hook

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
-import { DarkProvider, DarkContext } from "../contexts/DarkContext";
-import { ResumeProvider, ResumeContext } from "../contexts/ResumeContext";
+import { DarkContext } from "../contexts/DarkContext";
+import { ResumeContext } from "../contexts/ResumeContext";
 
 import {
 	About,
@@ -22,48 +22,41 @@ export default function Main() {
 	const [showResume, setShowResume] = useState(true);
 	const [isJson, setIsJson] = useState(false);
 	const resume = useContext(ResumeContext);
+	const { isDarkMode } = useContext(DarkContext);
 
 	const showJson = () => {
 		return showResume ? setIsJson(!isJson) : null;
 	};
 
 	return (
-		<ResumeProvider>
-			<DarkProvider>
-				<DarkContext.Consumer>
-					{({ isDarkMode }) => (
-						<div className={isDarkMode ? "main dark" : "main"}>
-							<Header showJson={showJson} />
-							{!isJson && (
-								<Switcher
-									showResume={showResume}
-									setShowResume={setShowResume}
-									showJson={showJson}
-								/>
-							)}
-							{!isJson && showResume && (
-								<>
-									<Contact />
-									<About />
-									<Profiles />
-									<Work />
-									<Education />
-									<Volunteer />
-									<Skills />
-									<Languages />
-									<Interests />
-								</>
-							)}
-							{!showResume && <CaseStudy />}
-							{isJson && (
-								<div className="pre">
-									<pre>{JSON.stringify(resume, null, 2)}</pre>
-								</div>
-							)}
-						</div>
-					)}
-				</DarkContext.Consumer>
-			</DarkProvider>
-		</ResumeProvider>
+		<div className={isDarkMode ? "main dark" : "main"}>
+			<Header showJson={showJson} />
+			{!isJson && (
+				<Switcher
+					showResume={showResume}
+					setShowResume={setShowResume}
+					showJson={showJson}
+				/>
+			)}
+			{!isJson && showResume && (
+				<>
+					<Contact />
+					<About />
+					<Profiles />
+					<Work />
+					<Education />
+					<Volunteer />
+					<Skills />
+					<Languages />
+					<Interests />
+				</>
+			)}
+			{!showResume && <CaseStudy />}
+			{isJson && (
+				<div className="pre">
+					<pre>{JSON.stringify(resume, null, 2)}</pre>
+				</div>
+			)}
+		</div>
 	);
 }
