9487fb93027806c308b03af5b980787753295b90
Joe Lee <jlee@knitpeople.com>
2021-05-19 10:30:31 -0400

    updated resume with Knit people

--- src/components/common/resume.js
+++ src/components/common/resume.js
@@ -1,227 +1,253 @@
 const RESUME = {
-  "basics": {
-    "name": "Kermit Joseph (Joe) Lee",
-    "label": "Web & Mobile Developer",
-    "picture": "https://avatars.githubusercontent.com/u/37561135?s=400&u=51704ccfc708e3b25c4fa2a05809df0cda88e5a3&v=4",
-    "email": "kermitjosephlee@gmail.com",
-    "phone": "(416) 799-1791",
-    "website": "http://www.kjlee.ca",
-    "summary": "A naturally curious developer, Joe made the transition from cooking in fine dining restaurants in Montreal and Boston to building better web and mobile integrated apps and learning to be a better coder.  You can find him in his offline time playing basketball, riding his bike, and fiddling with his sous-vide cooker.",
-    "location": {
-      "address": "116 Lisgar St.",
-      "postalCode": "ON M6J 3G3",
-      "city": "Toronto",
-      "countryCode": "CA",
-      "region": "Ontario"
-    },
-    "profiles": [
-      {
-        "network": "GitHub",
-        "username": "kermitjosephlee",
-        "url": "https://github.com/kermitjosephlee"
-      },
-      {
-        "network": "LinkedIn",
-        "username": "kermitjosephlee",
-        "url": "https://www.linkedin.com/in/kermit-joseph-lee-5524261a/"
-      }]
-  },
-  "work": [
-      {
-          "company": "FlipGive.com",
-          "position": "React & React Native Developer",
-          "website": "https://www.flipgive.com",
-          "startDate": "2019-09-30",
-          "endDate": "2021-02-22",
-          "isCodeRelated": true,         
-          "summary": "Online Shopping cashback web and mobile app focused on team and organizational fundraising.",
-          "highlights": [
-              "Working on building rich feature sets in ever expanding software suite of applications",
-              "Increased player participation by 100% by rebuilding player management pages",
-              "Built full feature sets for both web and mobile with React and React Native through GraphQL on Ruby on Rails"
-          ]
-      },
-      {
-        "company": "Lighthouse Labs",
-        "position": "Mentor",
-        "website": "https://www.lighthouselabs.ca/",
-        "startDate": "2020-09-15",
-        "endDate": "2021-12-31",
-        "isCodeRelated": true,
-        "summary": "Helping new coders at bootcamp with assignments and projects",
-        "highlights": [
-            "Kept grounded by fundamentals and giving back to new coders",
-            "Ability to walk through novice code, debug, and explain its workings"
-        ]
-      },
-      {
-        "company": "PlayLine.com",
-        "position": "React & React Native Developer",
-        "website": "https://playline.com",
-        "startDate": "2019-01-15",
-        "endDate": "2019-09-23",
-        "isCodeRelated": true,
-        "summary": "Sports Gaming & Entertainment Website founded by UFC Champion Michael Bisping and NBA All-Star Roy Hibbert",
-        "highlights": [
-          "Implemented site-wide design upgrades and launched new user-centric features",
-          "Building in React & ReactNative on PHP/MySQL"
-        ]
-      },
-      {
-        "company": "Freelance Contractor",
-        "position": "React Web Developer",
-        "startDate": "2018-10-01",
-        "endDate": "2018-12-30",
-        "isCodeRelated": true,
-        "summary": "Proprietary Tax Law Recertification Web App",
-        "highlights": [
-          "Built in React, Pose, Phoenix/Elixir"
-        ]
-      },
-      {
-        "company": "Edible Balloons",
-        "position": "Principal",
-        "website": "https://www.edibleballoons.com",
-        "startDate": "2016-04-01",
-        "endDate": "2018-09-30",
-        "isCodeRelated": false,
-        "summary": "Specializes in catering edible inflatable candy for events",
-        "highlights": [
-          "Collaborating with event managers and their clients to deliver experiences tailored to their specifications",
-          "Training and managing staff to deliver exceptional experiences"
-        ]
-      },
-      {
-        "company": "Louie Craft Coffee",
-        "position": "Manager",
-        "website": "http://louiecoffee.com",
-        "startDate": "2016-04-01",
-        "endDate": "2018-06-01",
-        "isCodeRelated": false,
-        "summary": "Managing an independent coffee shop in the historic Liberty Village Carpet Factory",
-        "highlights": [
-          "Overseeing daily operations, cash handling, ordering",
-          "Focused on team building and levelling up team members' skill sets",
-          "Specializing in consistent excellent customer experiences"
-        ]
-      },
-      {
-        "company": "Future Food Studio",
-        "position": "Chef Scientist",
-        "startDate": "2015-04-01",
-        "endDate": "2016-04-01",
-        "isCodeRelated": false,
-        "summary": "Created leading-edge food products for Fortune 500 compaines",
-        "highlights": [
-          "Launched new product offerings at Maker Faire NYC & Art Basel Miami",
-          "Experience in writing SOPs and coordinated on-site training to execute production",
-          "Collaborated in client-facing role with project management"
-        ]
-      },
-      {
-        "company": "Starbucks Coffee Canada",
-        "position": "Manager",
-        "website": "https://www.starbucks.ca",
-        "startDate": "2016-04-01",
-        "endDate": "2018-06-01",
-        "isCodeRelated": false,
-        "highlights": [
-          "Automated Labour Scheduling, Monthly Status Report, Quarterly Business Review, cash and inventory management",
-          "Business Planning including but not limited to scheduling, ordering, adjusting targets and tracking sales, developing partners. Created strategic and operational plans for the work group, managing execution, and measuring results."
-        ]
-      },
-      {
-        "company": "No. 9 Park - Boston",
-        "position": "Line Chef",
-        "startDate": "2008-07-01",
-        "endDate": "2009-05-01",
-        "isCodeRelated": false,
-      },
-      {
-        "company": "Le Club Chasse et Peche - Montreal",
-        "position": "Entremetier",
-        "startDate": "2006-09-01",
-        "endDate": "2008-06-30",
-        "isCodeRelated": false,
-      }
-    ],
-  "volunteer": [
-      {
-        "organization": "Bike Sauce",
-        "position": "Volunteer Coordinator",
-        "website": "http://bikesauce.org/",
-        "startDate": "2010-02-01",
-        "endDate": "2011-07-01",
-        "summary": "A DIY bike shop in Toronto. Empowering people by teaching people to fix their bicycles"
-      },
-      {
-        "organization": "Ride To Conquer Cancer",
-        "position": "Crew Captain - Bike Tech",
-        "website": "http://www.conquercancer.ca/",
-        "startDate": "2010-06-15",
-        "endDate": "2017-06-15",
-        "summary": "Helping riders with their bikes to raise money for cancer research"
-      }
-    ],
-  "education": [{
-      "institution": "Lighthouse Labs",
-      "area": "Full Stack Web Development",
-      "studyType": "Diploma",
-      "startDate": "2018-07-01",
-      "endDate": "2018-09-15"
-    },
-    {
-      "institution": "McGill University",
-      "area": "Chemistry",
-      "studyType": "Bachelor of Science",
-      "startDate": "1997-09-01",
-      "endDate": "2003-05-01"
-    },
-    {
-      "institution": "Le Cordon Bleu",
-      "area": "Cuisine",
-      "studyType": "Grand Diplome",
-      "startDate": "2004-09-01",
-      "endDate": "2005-05-01"
-    }
-  ],
-  "awards": [{
-    "title": "People's Choice Award",
-    "date": "2018-09-15",
-    "awarder": "Lighthouse Labs - Demo Day",
-    "summary": "Awarded for best demonstration project - PitCrew"
-  }],
-  "skills": {
-    "name": "Web Development",
-    "level": "Intermediate",
-    "keywords": [
-      "React",
-      "ReactNative",
-      "GraphQL/Apollo",
-      "Typescript",
-      "Javascript",
-      "Ruby on Rails",
-      "Docker",
-      "Git",
-      "HTML",
-      "CSS",
-      "NodeJS",
-      "Sass",
-      "BootStrap",
-    ]
-  },
-  "languages": [{
-    "language": "English",
-    "fluency": "Native speaker"
-  }],
-  "interests": [{
-    "keywords": [
-      "Cycling",
-      "Cooking",
-      "Basketball - Go Raptors!"
-    ]
-  }],
-  "caseStudy": [{ "paginatationCaseStudy": `
+	basics: {
+		name: "Kermit Joseph (Joe) Lee",
+		label: "Web & Mobile Developer",
+		picture:
+			"https://avatars.githubusercontent.com/u/37561135?s=400&u=51704ccfc708e3b25c4fa2a05809df0cda88e5a3&v=4",
+		email: "kermitjosephlee@gmail.com",
+		phone: "(416) 799-1791",
+		website: "http://www.kjlee.ca",
+		summary:
+			"A naturally curious developer, Joe made the transition from cooking in fine dining restaurants in Montreal and Boston to building better web and mobile integrated apps and learning to be a better coder.  You can find him in his offline time playing basketball, riding his bike, and fiddling with his sous-vide cooker.",
+		location: {
+			address: "116 Lisgar St.",
+			postalCode: "ON M6J 3G3",
+			city: "Toronto",
+			countryCode: "CA",
+			region: "Ontario",
+		},
+		profiles: [
+			{
+				network: "GitHub",
+				username: "kermitjosephlee",
+				url: "https://github.com/kermitjosephlee",
+			},
+			{
+				network: "LinkedIn",
+				username: "kermitjosephlee",
+				url: "https://www.linkedin.com/in/kermit-joseph-lee-5524261a/",
+			},
+		],
+	},
+	work: [
+		{
+			company: "Knit People",
+			position: "Front-End Developer",
+			website: "https://www.knitpeople.com",
+			startDate: `${new Date(2021, 2, 15)}`,
+			endDate: `${new Date()}`,
+			isCodeRelated: true,
+			summary: "Payroll and Human Resources SaaS company.",
+			highlights: [
+				"Collaborated with design team to improve UX of onboarding features",
+				"Built preliminary feature sets for web with React with Mocha and Cypress testing suites",
+			],
+		},
+
+		{
+			company: "Lighthouse Labs",
+			position: "Mentor",
+			website: "https://www.lighthouselabs.ca/",
+			startDate: `${new Date(2020, 8, 15)}`,
+			endDate: `${new Date()}`,
+			isCodeRelated: true,
+			summary: "Helping new coders at bootcamp with assignments and projects",
+			highlights: [
+				"Kept grounded by fundamentals and giving back to new coders",
+				"Ability to walk through novice code, debug, and explain its workings",
+			],
+		},
+		{
+			company: "FlipGive.com",
+			position: "React & React Native Developer",
+			website: "https://www.flipgive.com",
+			startDate: `${new Date(2019, 8, 30)}`,
+			endDate: `${new Date(2021, 1, 22)}`,
+			isCodeRelated: true,
+			summary:
+				"Online Shopping cashback web and mobile app focused on team and organizational fundraising.",
+			highlights: [
+				"Working on building rich feature sets in ever expanding software suite of applications",
+				"Increased player participation by 100% by rebuilding player management pages",
+				"Built full feature sets for both web and mobile with React and React Native through GraphQL on Ruby on Rails",
+			],
+		},
+		{
+			company: "PlayLine.com",
+			position: "React & React Native Developer",
+			website: "https://playline.com",
+			startDate: "2019-01-15",
+			endDate: "2019-09-23",
+			isCodeRelated: true,
+			summary:
+				"Sports Gaming & Entertainment Website founded by UFC Champion Michael Bisping and NBA All-Star Roy Hibbert",
+			highlights: [
+				"Implemented site-wide design upgrades and launched new user-centric features",
+				"Building in React & ReactNative on PHP/MySQL",
+			],
+		},
+
+		{
+			company: "Freelance Contractor",
+			position: "React Web Developer",
+			startDate: "2018-10-01",
+			endDate: "2018-12-30",
+			isCodeRelated: true,
+			summary: "Proprietary Tax Law Recertification Web App",
+			highlights: ["Built in React, Pose, Phoenix/Elixir"],
+		},
+		{
+			company: "Edible Balloons",
+			position: "Principal",
+			website: "https://www.edibleballoons.com",
+			startDate: "2016-04-01",
+			endDate: "2018-09-30",
+			isCodeRelated: false,
+			summary: "Specializes in catering edible inflatable candy for events",
+			highlights: [
+				"Collaborating with event managers and their clients to deliver experiences tailored to their specifications",
+				"Training and managing staff to deliver exceptional experiences",
+			],
+		},
+		{
+			company: "Louie Craft Coffee",
+			position: "Manager",
+			website: "http://louiecoffee.com",
+			startDate: "2016-04-01",
+			endDate: "2018-06-01",
+			isCodeRelated: false,
+			summary:
+				"Managing an independent coffee shop in the historic Liberty Village Carpet Factory",
+			highlights: [
+				"Overseeing daily operations, cash handling, ordering",
+				"Focused on team building and levelling up team members' skill sets",
+				"Specializing in consistent excellent customer experiences",
+			],
+		},
+		{
+			company: "Future Food Studio",
+			position: "Chef Scientist",
+			startDate: "2015-04-01",
+			endDate: "2016-04-01",
+			isCodeRelated: false,
+			summary: "Created leading-edge food products for Fortune 500 compaines",
+			highlights: [
+				"Launched new product offerings at Maker Faire NYC & Art Basel Miami",
+				"Experience in writing SOPs and coordinated on-site training to execute production",
+				"Collaborated in client-facing role with project management",
+			],
+		},
+		{
+			company: "Starbucks Coffee Canada",
+			position: "Manager",
+			website: "https://www.starbucks.ca",
+			startDate: "2016-04-01",
+			endDate: "2018-06-01",
+			isCodeRelated: false,
+			highlights: [
+				"Automated Labour Scheduling, Monthly Status Report, Quarterly Business Review, cash and inventory management",
+				"Business Planning including but not limited to scheduling, ordering, adjusting targets and tracking sales, developing partners. Created strategic and operational plans for the work group, managing execution, and measuring results.",
+			],
+		},
+		{
+			company: "No. 9 Park - Boston",
+			position: "Line Chef",
+			startDate: "2008-07-01",
+			endDate: "2009-05-01",
+			isCodeRelated: false,
+		},
+		{
+			company: "Le Club Chasse et Peche - Montreal",
+			position: "Entremetier",
+			startDate: "2006-09-01",
+			endDate: "2008-06-30",
+			isCodeRelated: false,
+		},
+	],
+	volunteer: [
+		{
+			organization: "Bike Sauce",
+			position: "Volunteer Coordinator",
+			website: "http://bikesauce.org/",
+			startDate: "2010-02-01",
+			endDate: "2011-07-01",
+			summary:
+				"A DIY bike shop in Toronto. Empowering people by teaching people to fix their bicycles",
+		},
+		{
+			organization: "Ride To Conquer Cancer",
+			position: "Crew Captain - Bike Tech",
+			website: "http://www.conquercancer.ca/",
+			startDate: "2010-06-15",
+			endDate: "2017-06-15",
+			summary:
+				"Helping riders with their bikes to raise money for cancer research",
+		},
+	],
+	education: [
+		{
+			institution: "Lighthouse Labs",
+			area: "Full Stack Web Development",
+			studyType: "Diploma",
+			startDate: "2018-07-01",
+			endDate: "2018-09-15",
+		},
+		{
+			institution: "McGill University",
+			area: "Chemistry",
+			studyType: "Bachelor of Science",
+			startDate: "1997-09-01",
+			endDate: "2003-05-01",
+		},
+		{
+			institution: "Le Cordon Bleu",
+			area: "Cuisine",
+			studyType: "Grand Diplome",
+			startDate: "2004-09-01",
+			endDate: "2005-05-01",
+		},
+	],
+	awards: [
+		{
+			title: "People's Choice Award",
+			date: "2018-09-15",
+			awarder: "Lighthouse Labs - Demo Day",
+			summary: "Awarded for best demonstration project - PitCrew",
+		},
+	],
+	skills: {
+		name: "Web Development",
+		level: "Intermediate",
+		keywords: [
+			"React",
+			"ReactNative",
+			"GraphQL/Apollo",
+			"Typescript",
+			"Javascript",
+			"Ruby on Rails",
+			"Docker",
+			"Git",
+			"HTML",
+			"CSS",
+			"NodeJS",
+			"Sass",
+			"BootStrap",
+		],
+	},
+	languages: [
+		{
+			language: "English",
+			fluency: "Native speaker",
+		},
+	],
+	interests: [
+		{
+			keywords: ["Cycling", "Cooking", "Basketball - Go Raptors!"],
+		},
+	],
+	caseStudy: [
+		{
+			paginatationCaseStudy: `
   ### A Story About Pagination
   
   It isn't anything flashy but it was a pagination feature. The standard treatment of pagination is that you have a flat list and use a cursor string to go back to the API to ask for more and give it the cursor so it knows where you are on that list. The API would then send back more.
@@ -238,8 +264,9 @@ const RESUME = {
   
   This also meant that the front-end filtering and conditional rendering needed thorough testing to make sure that dead-end use situations weren't occurring. The whole process to get this working took input from all levels of our product team. Design wanted to know what it would look like, what were the user flow views. Front-end wanted to know how this was going to affect the code and older apps. Back-end wanted to know if I was building the endpoints to avoid n+1's and testing the code properly before pushing for builds. There were conversations about whether to code the filtering and sorting in SQL or in Ruby (we went with SQL).
   
-  In the end, showing this at demos it wasn't flashy. Just a link that would have a spinner shown while loading and then the list would be appended. But it is a durable solution allowing for an infinite amount of fundraisers within a player group.`
-  }]
-}
+  In the end, showing this at demos it wasn't flashy. Just a link that would have a spinner shown while loading and then the list would be appended. But it is a durable solution allowing for an infinite amount of fundraisers within a player group.`,
+		},
+	],
+};
 
-export default RESUME
+export default RESUME;
