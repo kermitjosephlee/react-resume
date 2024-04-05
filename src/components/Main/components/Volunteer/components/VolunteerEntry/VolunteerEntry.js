f9d12066194b258c7b7ef8f62651098e83cdbcd3
Kermit Joseph Lee <37561135+kermitjosephlee@users.noreply.github.com>
2024-04-04 20:14:46 -0400

    removed dates from volunteer

--- src/components/Main/components/Volunteer/components/VolunteerEntry/VolunteerEntry.js
+++ src/components/Main/components/Volunteer/components/VolunteerEntry/VolunteerEntry.js
@@ -1,15 +1,11 @@
-import React, { useState } from "react";
-import { intervalMaker } from "../../../../../common/intervalMaker";
+import React from 'react';
 import "../../../Work/components/WorkEntry/WorkEntry.css";
 
 export function VolunteerEntry({ volunteerEntry }) {
-	const [isUnixTime, setIsUnitTime] = useState(false);
 	const {
 		organization,
 		position,
 		website,
-		startDate,
-		endDate,
 		summary,
 	} = volunteerEntry;
 
@@ -23,9 +19,9 @@ export function VolunteerEntry({ volunteerEntry }) {
 				<div className="website">
 					{!!website && <a href={website}>{website}</a>}
 				</div>
-				<div className="interval" onClick={() => setIsUnitTime(!isUnixTime)}>
+				{/* <div className="interval" onClick={() => setIsUnitTime(!isUnixTime)}>
 					{intervalMaker(startDate, endDate, isUnixTime)}
-				</div>
+				</div> */}
 			</div>
 			<ul className="bottom-container">
 				{!!summary && <li className="list-item">{summary}</li>}
